0d03bf30e6abaae512dac8d7fea41e27a252a137
safa <safasalahmousa@gmail.com>
2026-04-07 18:28:37 +0200

    add comments

--- src/services/paymentServics.js
+++ src/services/paymentServics.js
@@ -1,5 +1,11 @@
 import axiosInstance from "../api/axiosInstance"; 
 import { ENDPOINTS } from "../api/endpoints"; 
+/**
+ * Sends a request to the backend to create a Stripe Payment Intent.
+ * This is the first step in the payment process to secure a transaction.
+  * @param {Object} paymentData - An object containing necessary information for creating a payment intent, such as voucher code and booking ID.
+ * @returns {Promise<Object>} The payment intent data containing the clientSecret.
+ */
 
 export const createPaymentIntent = async (paymentData) => {
   // paymentData: { voucherCode, bookingId }
