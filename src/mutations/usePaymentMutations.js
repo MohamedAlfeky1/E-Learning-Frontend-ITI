731d09f79c635a48d95a5c8b8687e6f2d35b4092
safa <safasalahmousa@gmail.com>
2026-04-07 18:28:37 +0200

    add comments

--- src/mutations/usePaymentMutations.js
+++ src/mutations/usePaymentMutations.js
@@ -1,13 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
 import { createPaymentIntent } from "../services/paymentServics"; 
+import { toast } from "sonner";
+/**
+ * Custom hook to manage the payment checkout process.
+ * Triggers the creation of a Stripe Payment Intent and handles 
+ * the success and error states of the transaction.
+ */
 export const useCheckoutMutation = () => {
   return useMutation({
     mutationFn: (paymentData) => createPaymentIntent(paymentData),
-    onSuccess: (data) => {
-      console.log("Stripe Intent Created:", data.clientSecret);
-    },
     onError: (error) => {
-      console.error("Checkout Error:", error);
+      toast.error(error?.response?.data?.message || "Failed to initialize payment");
     }
   });
 };
