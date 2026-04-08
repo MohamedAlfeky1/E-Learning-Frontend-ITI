0d2184d35904671cc40b819887be339a15c758a9
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-04-08 18:29:28 +0200

    feat: add courses section in homepage

--- src/components/homepage/courses/index.jsx
+++ src/components/homepage/courses/index.jsx
@@ -12,8 +12,8 @@ import {
   PaginationLink,
 } from "@/components/ui/pagination";
 import { useState, useEffect } from "react";
-import Sigma from "../../../assets/homepage/categories/sigma.svg";
 import "./style.css";
+import CourseCard from "./CourseCard";
 
 const Courses = () => {
   const [api, setApi] = useState();
@@ -22,16 +22,24 @@ const Courses = () => {
   useEffect(() => {
     if (!api) return;
 
-    api.on("select", () => {
+    const onSelect = () => {
       setCurrent(api.selectedScrollSnap());
-    });
+    };
+
+    api.on("select", onSelect);
+
+    return () => {
+      api.off("select", onSelect);
+    };
   }, [api]);
 
   return (
     <section>
       <Carousel setApi={setApi} className="flex flex-col gap-10">
         <div className="courses-heading flex justify-between">
-          <h2 className="courses-heading-title text-[28px] sm:text-[36px]">Top Rated Courses</h2>
+          <h2 className="courses-heading-title text-[28px] sm:text-[36px]">
+            Top Rated Courses
+          </h2>
           <div className="arrows-container flex gap-2">
             <CarouselPrevious className="static translate-y-0" />
             <CarouselNext className="static translate-y-0" />
@@ -39,65 +47,26 @@ const Courses = () => {
         </div>
 
         <CarouselContent>
-          <CarouselItem>
-            <div
-              className="category-card p-8 rounded-2xl text-wrap flex flex-col gap-4"
-              style={{ backgroundColor: "#F1F3FF" }}
-            >
-              <div className="category-icon w-[56px] h-[56px] bg-white flex justify-center items-center rounded-lg">
-                <img src={Sigma} />
-              </div>
-              <h3 className="category-title">Mathematics</h3>
-              <p className="category-description">
-                From Algebra to Advanced Calculus and beyond.
-              </p>
-              <p className="category-num-courses">42 COURSES</p>
-            </div>
+          <CarouselItem className="lg:basis-2/5 xl:basis-1/3">
+            <CourseCard />
           </CarouselItem>
-          <CarouselItem>
-            <div
-              className="category-card p-8 rounded-2xl text-wrap flex flex-col gap-4"
-              style={{ backgroundColor: "#F1F3FF" }}
-            >
-              <div className="category-icon w-[56px] h-[56px] bg-white flex justify-center items-center rounded-lg">
-                <img src={Sigma} />
-              </div>
-              <h3 className="category-title">Mathematics</h3>
-              <p className="category-description">
-                From Algebra to Advanced Calculus and beyond.
-              </p>
-              <p className="category-num-courses">42 COURSES</p>
-            </div>
+          <CarouselItem className="lg:basis-2/5 xl:basis-1/3">
+            <CourseCard />
           </CarouselItem>
-          <CarouselItem>
-            <div
-              className="category-card p-8 rounded-2xl text-wrap flex flex-col gap-4"
-              style={{ backgroundColor: "#F1F3FF" }}
-            >
-              <div className="category-icon w-[56px] h-[56px] bg-white flex justify-center items-center rounded-lg">
-                <img src={Sigma} />
-              </div>
-              <h3 className="category-title">Mathematics</h3>
-              <p className="category-description">
-                From Algebra to Advanced Calculus and beyond.
-              </p>
-              <p className="category-num-courses">42 COURSES</p>
-            </div>
+          <CarouselItem className="lg:basis-2/5 xl:basis-1/3">
+            <CourseCard />
           </CarouselItem>
-          <CarouselItem>
-            <div
-              className="category-card p-8 rounded-2xl text-wrap flex flex-col gap-4"
-              style={{ backgroundColor: "#F1F3FF" }}
-            >
-              <div className="category-icon w-[56px] h-[56px] bg-white flex justify-center items-center rounded-lg">
-                <img src={Sigma} />
-              </div>
-              <h3 className="category-title">Mathematics</h3>
-              <p className="category-description">
-                From Algebra to Advanced Calculus and beyond.
-              </p>
-              <p className="category-num-courses">42 COURSES</p>
-            </div>
+          <CarouselItem className="lg:basis-2/5 xl:basis-1/3">
+            <CourseCard />
+          </CarouselItem>
+          <CarouselItem className="lg:basis-2/5 xl:basis-1/3">
+            <CourseCard />
+          </CarouselItem>
+          <CarouselItem className="lg:basis-2/5 xl:basis-1/3">
+            <CourseCard />
+          </CarouselItem>
+          <CarouselItem className="lg:basis-2/5 xl:basis-1/3">
+            <CourseCard />
           </CarouselItem>
         </CarouselContent>
         <Pagination>
@@ -105,62 +74,40 @@ const Courses = () => {
             <PaginationItem>
               <PaginationLink
                 href="#"
-                className="pagination-link rounded-full"
+                className={`pagination-link rounded-full ${current === 0 ? "active" : ""}`}
                 onClick={(event) => {
                   event.preventDefault();
                   api.scrollTo(0);
-                  if (current === 0) {
-                    event.target.classList.add("active");
-                  } else {
-                    event.target.classList.remove("active");
-                  }
                 }}
               ></PaginationLink>
             </PaginationItem>
             <PaginationItem>
               <PaginationLink
                 href="#"
-                className="pagination-link rounded-full"
+                className={`pagination-link rounded-full ${current === 1 ? "active" : ""}`}
                 onClick={(event) => {
                   event.preventDefault();
                   api.scrollTo(1);
-                  if (current === 1) {
-                    event.target.classList.add("active");
-                    console.log(event.target.className);
-                  } else {
-                    event.target.classList.remove("active");
-                    console.log(event.target.className);
-                  }
                 }}
               ></PaginationLink>
             </PaginationItem>
             <PaginationItem>
               <PaginationLink
                 href="#"
-                className="pagination-link rounded-full"
+                className={`pagination-link rounded-full ${current === 2 ? "active" : ""}`}
                 onClick={(event) => {
                   event.preventDefault();
                   api.scrollTo(2);
-                  if (current === 3) {
-                    event.target.classList.add("active");
-                  } else {
-                    event.target.classList.remove("active");
-                  }
                 }}
               ></PaginationLink>
             </PaginationItem>
             <PaginationItem>
               <PaginationLink
                 href="#"
-                className="pagination-link rounded-full"
+                className={`pagination-link rounded-full ${current === 3 ? "active" : ""}`}
                 onClick={(event) => {
                   event.preventDefault();
                   api.scrollTo(3);
-                  if (current === 3) {
-                    event.target.classList.add("active");
-                  } else {
-                    event.target.classList.remove("active");
-                  }
                 }}
               ></PaginationLink>
             </PaginationItem>
