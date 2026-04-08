bf0d50da84344eb4f5c0c1c9d3b41565de02fe42
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-04-08 18:29:28 +0200

    feat: add courses section in homepage

---
+++ src/components/homepage/courses/CourseCard.jsx
@@ -0,0 +1,34 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import CourseImg from "../../../assets/homepage/courses/course-img.png";
+import "./style.css";
+
+const CourseCard = () => {
+  return (
+    <div className="course-card w-full lg:w-[400px] p-4 bg-white rounded-4xl flex flex-col gap-6 ">
+      <img
+        src={CourseImg}
+        alt="Course Image"
+        className="course-img rounded-4xl"
+      />
+      <div className="course-details px-4 pb-4 flex flex-col gap-2">
+        <Badge
+          variant="ghost"
+          className="course-category uppercase text-[10px] leading-[15px]"
+        >
+          fine arts
+        </Badge>
+        <p className="course-title">Modernism & The Digital Canvas</p>
+        <p className="course-description">
+          Exploring the intersection of traditional painting and digital media.
+        </p>
+        <div className="price-details pt-4 flex justify-between items-end">
+          <p className="price">$49.99</p>
+          <Button className="details-btn px-4 py-2">Details</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseCard;
