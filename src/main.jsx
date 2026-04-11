6c9f88c5f0423b5a3e36f4967af737bc4b2d05a6
MohamedAlfeky1 <mohamedelfeky.dev@gmail.com>
2026-04-11 03:07:09 +0200

    Merge PR #7 (Quizes): Code review, resolve  and apply custom enhancements

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import { Toaster } from "@/components/ui/sonner";
-import router from "./router";
 import App from "./App";
 
 const queryClient = new QueryClient({
@@ -22,7 +20,6 @@ createRoot(document.getElementById("root")).render(
     <GoogleOAuthProvider clientId="136478097339-2bi830r415h06g5jp67hn3bht90s8t1s.apps.googleusercontent.com">
       <QueryClientProvider client={queryClient}>
         <Toaster position="top-right" richColors closeButton />
-        <RouterProvider router={router} />
         <App />
       </QueryClientProvider>
     </GoogleOAuthProvider>
