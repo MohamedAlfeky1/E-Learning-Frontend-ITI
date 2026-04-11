c4b579b68f35108a3628dc96bf1136b403ec167b
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-04-11 19:17:21 +0200

    feat: add course card component to cart page

---
+++ src/pages/student/CartPage/CourseCard.jsx
@@ -0,0 +1,31 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import CourseImg from "../../../assets/homepage/courses/course-img.png";
+import "./CourseCard.css";
+
+const CourseCard = () => {
+  return (
+    <div className="p-4 bg-white rounded-4xl flex items-center gap-6 hover:scale-101 duration-150">
+      <img
+        src={CourseImg}
+        alt="Course Image"
+        className="h-[150px] rounded-xl"
+      />
+      <div className="grow flex flex-col justify-between">
+        <Badge
+          variant="ghost"
+          className="course-category uppercase text-[10px] leading-[15px]"
+        >
+          fine arts
+        </Badge>
+        <p className="course-title">Modernism & The Digital Canvas</p>
+        <div className="price-details pt-4 flex justify-between items-end">
+          <p className="price" style={{ color: "#3525CD" }}>$49.99</p>
+          <Button className="details-btn px-4 py-2">Details</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseCard;
