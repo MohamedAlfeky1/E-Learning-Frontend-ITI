876e4ce625aa9012c16a41a388ae9d7648180f5e
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-04-11 13:34:14 +0200

    feat: add header and upload button

--- src/pages/teacher/UploadMaterialPage/index.jsx
+++ src/pages/teacher/UploadMaterialPage/index.jsx
@@ -1,7 +1,45 @@
+import { CloudUpload, PlusIcon } from "lucide-react";
+import "./style.css";
+import { Button } from "@/components/ui/button";
+
 const UploadMaterialPage = () => {
   return (
-    <div>
-      <h1>UploadMaterialPage</h1>
+    <div className="min-h-screen text-white p-6 md:p-12 font-sans favorites-page-bg">
+      {/* Header */}
+      <header className="mb-8 flex flex-col justify-between">
+        <p className="tracking-widest uppercase mb-2 text-primary text-size-xs font-weight-700 font-inter">
+          MEDIA MANAGER
+        </p>
+        <h1 className="mb-2 text-dark text-size-3xl font-weight-800 font-plus-jakarta">
+          Upload Course Assets
+        </h1>
+        <p className="text-muted-foreground max-w-md text-secondary text-size-base font-weight-400 font-inter">
+          Manage your educational video library, presentation decks, and
+          supplementary materials. Supported formats: MP4, MOV, PDF, PPTX.
+        </p>
+      </header>
+
+      <section>
+        <div className="border-2 border-dashed border-slate-300 bg-white rounded-[24px] flex flex-col items-center justify-center gap-2 text-center p-12 cursor-pointer transition-colors hover:border-indigo-600">
+          <Button variant="secondary" className="size-16 rounded-full flex items-center justify-center mb-2">
+            <CloudUpload color="#3525CD" />
+          </Button>
+          <h3
+            className="text-slate-950 text-base font-bold leading-6 mb-0"
+            style={{ fontFamily: "'Plus Jakarta Sans', Inter, sans-serif" }}
+          >
+            Drag and drop your files
+          </h3>
+          <p className="text-slate-600 text-sm leading-6 mb-6">
+            Up to 2GB per file. High-resolution video recommended.
+          </p>
+          <Button
+            style={{ fontFamily: "'Plus Jakarta Sans', Inter, sans-serif" }}
+          >
+            Select Files
+          </Button>
+        </div>
+      </section>
     </div>
   );
 };
