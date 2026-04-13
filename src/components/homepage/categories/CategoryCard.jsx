e4488667d656cec395c44616e73fd93ca55aa177
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-04-13 15:56:39 +0200

    feat: display categories data received from the server

--- src/components/homepage/categories/CategoryCard.jsx
+++ src/components/homepage/categories/CategoryCard.jsx
@@ -1,20 +1,21 @@
-import Sigma from "../../../assets/homepage/categories/sigma.svg";
+// import Sigma from "../../../assets/homepage/categories/sigma.svg";
 import "./style.css";
 
-const CategoryCard = () => {
+const CategoryCard = ({ category }) => {
   return (
     <div
-      className="category-card w-75 p-8 rounded-2xl text-wrap flex flex-col gap-4"
+      className="category-card w-75 p-8 rounded-2xl text-wrap flex flex-col justify-between gap-4 hover:scale-101 duration-150 cursor-pointer"
       style={{ backgroundColor: "#F1F3FF" }}
     >
       <div className="category-icon w-[56px] h-[56px] bg-white flex justify-center items-center rounded-lg">
-        <img src={Sigma} />
+        {/* <img src={Sigma} /> */}
+        {category.icon}
       </div>
-      <h3 className="category-title">Mathematics</h3>
-      <p className="category-description">
+      <h3 className="category-title">{category.name}</h3>
+      {/* <p className="category-description">
         From Algebra to Advanced Calculus and beyond.
-      </p>
-      <p className="category-num-courses">42 COURSES</p>
+      </p> */}
+      <p className="category-num-courses">{category.courseCount} COURSES</p>
     </div>
   );
 };
