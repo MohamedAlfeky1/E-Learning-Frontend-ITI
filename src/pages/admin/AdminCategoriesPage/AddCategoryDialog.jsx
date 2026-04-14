ed8cb8baa4e4f5b2256358da510a683a24104a05
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-04-14 18:28:13 +0200

    feat: make create new category button functional
    - clicking on the button shows a dialog containing a form to add a new
      category.
    - control form using zod and react-hook-form.
    - made some stylistic changes to the categories page.

---
+++ src/pages/admin/AdminCategoriesPage/AddCategoryDialog.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axiosInstance from "@/api/axiosInstance";
+import { ENDPOINTS } from "@/api/endpoints";
+import { toast } from "sonner";
+
+const formSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(1, "Description is required"),
+});
+
+const AddCategoryDialog = () => {
+  const [open, setOpen] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
+
+  const onSubmit = async ({ name, description }) => {
+    try {
+      const response = await axiosInstance.post(ENDPOINTS.CATEGORIES_CREATE, {
+        name, description
+      });
+      setOpen(false);
+      toast.success(`Category ${name} has been added`);
+    } catch (error) {
+      console.log(error);
+      toast.error(`Failed to add category ${name}`);
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="w-full inline-flex items-center gap-2 rounded-[20px] bg-[#3525CD] px-6 py-4 text-white text-base font-bold transition-transform duration-150 hover:scale-[1.01]">
+          <CirclePlus />
+          Create New Category
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-w-sm">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogHeader>
+            <DialogTitle>Add New Category</DialogTitle>
+            <DialogDescription>
+              Fill out the details below to create a new category.
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="grid gap-4 py-4">
+            {/* Name Field */}
+            <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    id="name"
+                    className={errors.name ? "border-destructive" : ""}
+                  />
+                )}
+              />
+              {errors.name && (
+                <span className="text-sm text-red-500">
+                  {errors.name.message}
+                </span>
+              )}
+            </div>
+
+            {/* Description Field */}
+            <div className="grid gap-2">
+              <Label htmlFor="description">Description</Label>
+              <Controller
+                name="description"
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    id="description"
+                    className={errors.description ? "border-destructive" : ""}
+                  />
+                )}
+              />
+              {errors.description && (
+                <span className="text-sm text-red-500">
+                  {errors.description.message}
+                </span>
+              )}
+            </div>
+          </div>
+
+          <DialogFooter>
+            <Button type="submit">Add Category</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddCategoryDialog;
