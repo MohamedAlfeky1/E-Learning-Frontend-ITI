7b70d0e407fa3b57dbd6c722c02b1e215afbbb0d
safa <safasalahmousa@gmail.com>
2026-04-14 16:37:56 +0200

    edit text color

--- src/pages/admin/StudentProfilePage/index.jsx
+++ src/pages/admin/StudentProfilePage/index.jsx
@@ -93,7 +93,7 @@ const StudentProfilePage = () => {
       <Card className="rounded-3xl shadow-md">
         <CardContent className="p-4 md:p-6 space-y-4">
 
-          <h3 className="text-lg md:text-xl font-bold">
+          <h3 className="text-lg md:text-xl font-bold text-primary">
             Enrolled Courses ({courses.length})
           </h3>
 
