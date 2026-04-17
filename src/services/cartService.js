045ddbf764806e7c7bfe21e2a3adf329a9f2265d
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-04-17 23:05:57 +0200

    feat: receive and display error message received from server

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -2,20 +2,32 @@ import axiosInstance from "@/api/axiosInstance";
 import { ENDPOINTS } from "@/api/endpoints";
 
 export const getCart = async () => {
-  const response = await axiosInstance.get(ENDPOINTS.CART_GET);
-  return response.data;
+  try {
+    const response = await axiosInstance.get(ENDPOINTS.CART_GET);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
 
 export const addToCart = async (courseId) => {
-  const response = await axiosInstance.post(ENDPOINTS.CART_ADD, {
-    courseId,
-  });
-  return response.data;
+  try {
+    const response = await axiosInstance.post(ENDPOINTS.CART_ADD, {
+      courseId,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
 
 export const removeFromCart = async (courseId) => {
-  const response = await axiosInstance.post(ENDPOINTS.CART_REMOVE, {
-    courseId,
-  });
-  return response.data;
+  try {
+    const response = await axiosInstance.post(ENDPOINTS.CART_REMOVE, {
+      courseId,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
