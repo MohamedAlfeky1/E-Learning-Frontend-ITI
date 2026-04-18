a6c1203c16055fb63a7c6a3eb4b3330385c90151
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-04-18 11:45:11 +0200

    feat: update services to send error response data

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -7,30 +7,49 @@ export const getAllCategories = async () => {
 };
 
 export const getCategoryById = async (id) => {
-  const response = await axiosInstance.get(ENDPOINTS.CATEGORY_BY_ID(id));
-  return response.data;
+  try {
+    const response = await axiosInstance.get(ENDPOINTS.CATEGORY_BY_ID(id));
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
 
 export const addCategory = async ({ name, description, icon }) => {
-  const response = await axiosInstance.post(ENDPOINTS.CATEGORIES_CREATE, {
-    name,
-    description,
-    icon,
-  });
-  return response.data;
+  try {
+    const response = await axiosInstance.post(ENDPOINTS.CATEGORIES_CREATE, {
+      name,
+      description,
+      icon,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
 
 export const updateCategory = async ({ slug, name, description }) => {
-  const response = await axiosInstance.put(ENDPOINTS.CATEGORIES_UPDATE(slug), {
-    name,
-    description,
-  });
-  return response.data;
+  try {
+    const response = await axiosInstance.put(
+      ENDPOINTS.CATEGORIES_UPDATE(slug),
+      {
+        name,
+        description,
+      },
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
 
 export const deleteCategory = async (slug) => {
-  const response = await axiosInstance.delete(
-    ENDPOINTS.CATEGORIES_DELETE(slug),
-  );
-  return response.data;
+  try {
+    const response = await axiosInstance.delete(
+      ENDPOINTS.CATEGORIES_DELETE(slug),
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
--- src/services/slidersService.js
+++ src/services/slidersService.js
@@ -2,26 +2,42 @@ import axiosInstance from "@/api/axiosInstance";
 import { ENDPOINTS } from "@/api/endpoints";
 
 export const getPublicSliders = async () => {
-  const response = await axiosInstance.get(ENDPOINTS.SLIDERS_LIST);
-  return response.data;
+  try {
+    const response = await axiosInstance.get(ENDPOINTS.SLIDERS_LIST);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
 
 export const getSliders = async () => {
-  const response = await axiosInstance.get(ENDPOINTS.ADMIN_SLIDERS_LIST);
-  return response.data;
+  try {
+    const response = await axiosInstance.get(ENDPOINTS.ADMIN_SLIDERS_LIST);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
 
 export const addSlider = async (data) => {
-  const response = await axiosInstance.post(
-    ENDPOINTS.ADMIN_SLIDERS_CREATE,
-    data,
-  );
-  return response.data;
+  try {
+    const response = await axiosInstance.post(
+      ENDPOINTS.ADMIN_SLIDERS_CREATE,
+      data,
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
 
 export const deleteSlider = async (id) => {
-  const response = await axiosInstance.delete(
-    ENDPOINTS.ADMIN_SLIDERS_DELETE(id),
-  );
-  return response.data;
+  try {
+    const response = await axiosInstance.delete(
+      ENDPOINTS.ADMIN_SLIDERS_DELETE(id),
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
