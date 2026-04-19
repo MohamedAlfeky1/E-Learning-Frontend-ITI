a54f913aeb4b9fc7fb7bbf8974ed2f49083984c3
Hadeer Abdelgawad <hadeer.abdelgawad44@gmail.com>
2026-04-19 23:45:54 +0200

    'bugsDone'

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -1,3 +1,4 @@
+
 import axiosInstance from "../api/axiosInstance";
 import { ENDPOINTS } from "../api/endpoints";
 
@@ -5,7 +6,7 @@ import { ENDPOINTS } from "../api/endpoints";
 export const getCourseById = async (id) => {
   try {
     const response = await axiosInstance.get(ENDPOINTS.COURSES_GET(id));
-    return response.data;
+    return response?.data;
   } catch (error) {
     console.error("Error fetching course:", error);
     throw error;
