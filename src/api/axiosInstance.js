4e2b7eb58a81f37f72436bfd2faee31a6a20ec4a
Hadeer Abdelgawad <hadeer.abdelgawad44@gmail.com>
2026-05-03 23:48:38 +0300

    'rowshandle'

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-learning-platform-api-production.up.railway.app/api",
 });
 axiosInstance.interceptors.request.use(
   (config) => {
