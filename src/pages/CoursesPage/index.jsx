015034f223402186620c3badb6a03c6b40bf43a5
MohamedAlfeky1 <mohamedelfeky.dev@gmail.com>
2026-05-03 12:12:52 +0300

    feat: implement base templates for Home, About, and Courses pages

--- src/pages/CoursesPage/index.jsx
+++ src/pages/CoursesPage/index.jsx
@@ -101,7 +101,7 @@ const CoursesPage = () => {
   }, [filters, sort, page]);
 
   return (
-    <div className="p-6">
+    <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
 
       {/* Hero */}
       <div className="flex flex-col gap-3">
